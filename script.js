26f22cd06c5b9fc6baac84fbc821801c8f57d8eb
yetimercury <annette.marie.mills@gmail.com>
2026-05-24 16:28:26 -0400

    Update script.js

--- script.js
+++ script.js
@@ -125,7 +125,7 @@ if (minutes >= task.start && minutes < task.end) {
 // UPCOMING TASKS
 if (minutes < task.start) {
   fill = task.color;
-  opacity = 0.38;
+  opacity = 0.12;
 }
 
     const path = document.createElementNS("http://www.w3.org/2000/svg", "path");
